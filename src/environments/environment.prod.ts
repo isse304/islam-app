33fa230a0c40da38dc489fb9d8a4d4232042079c
isse304 <101603508+isse304@users.noreply.github.com>
2025-03-12 07:40:14 -0500

    Update environment configurations and dependencies

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,19 +2,19 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   production: true,
-  apiUrl: 'https://your-production-api.com',
+  apiUrl: 'https://islam-app-backend.onrender.com',
   openaiApiKey: undefined,
   elevenLabsApiKey: undefined,
   sttApiKey: undefined,
-  clerkPublishableKey: 'pk_test_cm9idXN0LWNyYXdkYWQtNDcuY2xlcmsuYWNjb3VudHMuZGV2JA',
-  clerkFrontendApi: 'https://robust-crawdad-47.clerk.accounts.dev',
+  clerkPublishableKey: 'pk_live_Y2xlcmsubnVyYS1haS5hcHAk',
+  clerkFrontendApi: 'https://clerk.nura-ai.app',
   aws: {
     accessKeyId: undefined,
     secretAccessKey: undefined,
     region: 'us-east-1'
   },
   stripeConfig: {
-    publishableKey: process.env['STRIPE_PUBLISHABLE_KEY'] || '',
-    priceId: process.env['STRIPE_PRICE_ID'] || ''
+    publishableKey: 'pk_live_51R1RShGYeNehzlUZnehEoAkNzTKRO29KrBhHVlrJZVliO8MBrI9gHgbeSPL1ns7QOlO8vQ99afIl2EfAZ4HSoBFX00J8wRZMur',
+    priceId: 'price_1R1SKuGYeNehzlUZPlVwt392'
   }
 }; 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,28 +1,9 @@
-interface Environment {
-  production: boolean;
-  apiUrl: string;
-  openaiApiKey: string | undefined;
-  elevenLabsApiKey: string | undefined;
-  sttApiKey: string | undefined;
-  clerkPublishableKey: string;
-  clerkFrontendApi: string;
-  aws: {
-    accessKeyId: string | undefined;
-    secretAccessKey: string | undefined;
-    region: string;
-  };
-  stripeConfig: {
-    publishableKey: string;
-    priceId: string;
-  };
-}
-
-declare const process: any;
+import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   production: false,
   apiUrl: 'http://localhost:3000',
-  openaiApiKey: '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',
+  openaiApiKey: undefined,
   elevenLabsApiKey: undefined,
   sttApiKey: undefined,
   clerkPublishableKey: 'pk_test_cm9idXN0LWNyYXdkYWQtNDcuY2xlcmsuYWNjb3VudHMuZGV2JA',
@@ -33,7 +14,7 @@ export const environment: Environment = {
     region: 'us-east-1'
   },
   stripeConfig: {
-    publishableKey: 'pk_test_51R1RShGYeNehzlUZBgXi4s6sf5u4BvRkXP7YNb07aNwpYnaPl7VL9GJ4rnGdkLvWDYtd7Jg2w6NPmeXgifPCwd220010zxsvdj',
+    publishableKey: 'pk_test_51R1nqsGYeNehzlUZBgXi4s6sf5u4BvRkXP7YNb07aNwpYnaPl7VL9GJ4rnGdkLvWDYtd7Jg2w6NPmeXgifPCwd220010zxsvdj',
     priceId: 'price_1R1TPjGYeNehzlUZi71dNilr'
   }
 }; 
