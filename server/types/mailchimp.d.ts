758000ecee3e95f90d3df54bf95e8e7f743ee462
isse304 <101603508+isse304@users.noreply.github.com>
2025-04-07 13:48:54 -0500

    fixed mailchimp module error

---
+++ server/types/mailchimp.d.ts
@@ -0,0 +1,3 @@
+// This declaration file tells TypeScript to treat the Mailchimp module as 'any' 
+// when type definitions are not found or are incomplete, resolving build errors.
+declare module '@mailchimp/mailchimp_marketing'; 
